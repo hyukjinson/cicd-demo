d29242f0876b975dd2149c2d8d6787c8302d6226
hjson <hyukjin.son@lgcns.com>
2021-12-02 15:37:25 +0900

    test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var os = require('os');
 var http = require('http');
 var handleRequest = function(request, response) {
  response.writeHead(200);
- response.end("Hello Everyone! This is NCP Test_V1! I'm "+os.hostname());
+ response.end("Hello Everyone! This is NCP Test_V2! I'm "+os.hostname());
 
  //log
  console.log("["+
